368793c2971f27bcc106e86fbc84c847e1656cfb
Dex <sheepareawesome777@gmail.com>
2023-04-16 01:01:36 -0400

    Add `arrayEq`

--- index.ts
+++ index.ts
@@ -23,6 +23,19 @@ export namespace math {
 
 		return final;
 	}
+
+	// If arrays are equal, returns true.
+	export function arrayEq(arr1: any[], arr2: any[]) {
+		if (arr1.length !== arr2.length) return false;
+
+		for (let i in arr1) {
+			if (!(arr1[i] === arr2[i])) {
+				return false
+			}
+		}
+
+		return true;
+	}
 }
 
 export namespace qstr {
